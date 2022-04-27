15eafbb857e8af5c1c0f18a13aa557d7a5636d2c
Tom <101tuna@gmail.com>
2022-04-27 01:18:41 -0400

    Fix Test.js

--- Test.js
+++ Test.js
@@ -2,10 +2,9 @@
 
 function testLoad() {
     var url = document.URL;
+    var codeStr = url.searchParams.get("code");
 
     if (codeStr.length > 1) {
-        var codeStr = url.searchParams.get("code");
-
         document.getElementById("url").innerHTML = url;
         document.getElementById("code").innerHTML = codeStr;
     }
