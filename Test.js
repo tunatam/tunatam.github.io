39c2ef9438242a676c4aa78e51f574754676cdb9
Tom <101tuna@gmail.com>
2022-04-27 03:13:05 -0400

    Trying to get Access Token

--- Test.js
+++ Test.js
@@ -1,12 +1,19 @@
 
 
-function testLoad() {
+function onPageLoad() {
     var url = document.URL;
-    var parameters = getJsonFromUrl(url);
+    var baseUrl = "https://tunatam.github.io/";
+
+    if (url.length > baseUrl.length) {
+        var parameters = getJsonFromUrl(url);
+    }
 
     if (parameters["code"] != null) {
+        var code = parameters["code"]
         document.getElementById("url").innerHTML = url;
-        document.getElementById("code").innerHTML = parameters["code"];
+        document.getElementById("code").innerHTML = code;
+
+        getAccessToken(code);
     }
 }
 
@@ -19,4 +26,25 @@ function getJsonFromUrl(url) {
         result[item[0]] = decodeURIComponent(item[1]);
     });
     return result;
+}
+
+function getAccessToken(code) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://www.bungie.net/Platform/App/OAuth/Token/ HTTP/1.1");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+      }};
+
+    let data = `{
+      "client_id": 37970,
+      "grant_type": "authorization_code",
+      "code": code
+    }`;
+
+    xhr.send(data);
 }
