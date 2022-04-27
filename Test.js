12a6aab23f02f1d4c0eb705bdeb6d8db0059c878
Tom <101tuna@gmail.com>
2022-04-27 01:41:00 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,10 +2,26 @@
 
 function testLoad() {
     var url = document.URL;
-    var codeStr = url.searchParams.get("code");
+    var parameters = getJsonFromUrl(url);
 
-    if (codeStr.length > 1) {
+    if (parameters.length > 1) {
         document.getElementById("url").innerHTML = url;
-        document.getElementById("code").innerHTML = codeStr;
+
+        for (int i; i < parameters.length; i++) {
+            if (parameters[i].localeCompare("code") {
+                document.getElementById("code").innerHTML = parameters[i];
+            }
+        }
     }
+}
+
+function getJsonFromUrl(url) {
+    if(!url) url = document.URL;
+    var query = url.substring(1);
+    var result = {};
+    query.split("&").forEach(function(part) {
+        var item = part.split("=");
+        result[item[0]] = decodeURIComponent(item[1]);
+    });
+    return result;
 }
